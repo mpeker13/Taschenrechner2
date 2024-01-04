8c9c4f75e984f6e2d862fe73ec052086c6560280
Meryem Peker <meryem.peker@kinteo.de>
2024-01-04 15:42:59 +0100

    Erste Version vom Taschenrechnerprojekt

---
+++ Taschenrechner.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+public class Taschenrechner {
+    public static void main (String[] args ){
+        int zahl1;
+        int zahl2;
+        int ergebnis;
+
+        Scanner eingabe = new Scanner(System.in);
+
+        for(int i=1; i<10; i++) {
+
+            System.out.print("Erste Zahl eingeben: ");
+            zahl1 = eingabe.nextInt();
+
+            System.out.print("Zweite Zahl eingeben: ");
+            zahl2 = eingabe.nextInt();
+
+            /*Summieren*/
+            ergebnis = zahl1 + zahl2;
+            System.out.println("Ergebnis Summe= " + ergebnis);
+
+            /*Subtrahieren*/
+            ergebnis = zahl1 - zahl2;
+            System.out.println("Ergebnis= " + ergebnis);
+
+            /*Mal*/
+            ergebnis = zahl1 * zahl2;
+            System.out.println("Ergebnis= " + ergebnis);
+
+            /*Dividieren*/
+            ergebnis = zahl1 / zahl2;
+            System.out.println("Ergebnis= " + ergebnis);
+        }
+
+    }
+}
